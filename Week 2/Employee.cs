0a46732b6a55a84cfde1f37ebeb957ac80ffec23
furkancan demircan <furkancan.demircan@gmail.com>
2025-10-05 16:16:11 +0300

    Week 2 added!

---
+++ Week 2/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2
+{
+    public class Employee
+    {
+        private int _id = 0;
+        private string _firstName;
+        private string _lastName;
+        private string _department;
+        private DateTime _startDate;
+        private float _salary;
+
+        private Employee()
+        {
+            _salary = 0;
+            _startDate = DateTime.Now;
+        }
+
+        public Employee(string firstName, string lastName, string department)
+        : this()
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _department = department;
+        }
+
+        public string GetFullName() => _firstName + " " + _lastName;
+
+        public void SetSalary(float salary)
+        {
+            if (salary < 0)
+            {
+                _salary = 0;
+                return;
+            }
+            _salary = salary;
+        }
+
+        public void DisplayInfo() => Console.WriteLine($"{_id} {GetFullName()} {_department} {_salary} {_startDate.Date.ToShortDateString()}");
+    }
+}
---
+++ Week 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using Week_2;
+
+Employee emp1 = new Employee("Ali", "Çoban", "Software");
+emp1.SetSalary(100000);
+Employee emp2 = new Employee(lastName: "Kojima", firstName: "Hideo", department: "Game Development");
+emp2.SetSalary(75000);
+Employee emp3 = new Employee(firstName: "Can", "Bora", "AI");
+emp3.SetSalary(-150000);
+
+emp1.DisplayInfo();
+emp2.DisplayInfo();
+emp3.DisplayInfo();
